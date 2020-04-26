59887f3a55e60d9110c9a1df68120bf957ed3792
juliof9 <00081817@uca.edu.sv>
2020-04-26 02:09:50 -0600

    second commit

--- TareaLabo4/tarea_4_00081817.asm
+++ TareaLabo4/tarea_4_00081817.asm
@@ -1,1 +1,13 @@
     org     100h
+    ;Ejercicio1
+    ;carnet=00081817 = 25 = 25/5 = 5
+    ;Respuesta=5, "En el segundo"(Un clasico xd)
+
+    mov ax, 000h
+    add ax, 8d
+    add ax, 1d
+    add ax, 8d
+    add ax, 1d
+    add ax, 7d
+    mov cl, 5d
+    div cl
