bca8191b8f367aa7cb2f0ef08362577f037d4256
juliof9 <00081817@uca.edu.sv>
2020-04-26 18:32:43 -0600

    Le cambie nombre al archivo .asm porque no me dejaba correrlo pero hice un carpeta con ese formato

--- tarea_4_00081817/tarea4.asm
+++ tarea_4_00081817/tarea4.asm
@@ -59,4 +59,21 @@ next:   cmp bx, 21Fh
 
 ;Ejercicio3
 
-        
+    mov     ax, 0000h 
+    mov     bx, 0000h
+    mov     cx, 0000h
+    mov     ax, 0d
+    mov     di, 0d
+    mov     cx, 0015d
+    mov     dx, 1d
+
+    lupe:mov    [di + 220h], ax    
+    inc     di
+    
+    mov     bx,ax
+    add     ax, dx
+    mov     dx, bx
+
+    
+    loop    lupe
+    int 20h        
